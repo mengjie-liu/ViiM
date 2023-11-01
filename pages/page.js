d2c5a94615c6b01873bda977f372d31562e12073
mengjie-liu <mengjie.liu@yale.edu>
2023-11-01 19:07:54 -0400

    update work

--- pages/page.js
+++ pages/page.js
@@ -1,3 +1,18 @@
-// $(document).ready(funciton(){
-
-// })
+$(document).ready(function () {
+  $(".vid")
+    .children("svg")
+    .click(function () {
+      if ($(this).parent(".vid").siblings(".vid")) {
+        $(this)
+          .parent(".vid")
+          .siblings(".vid")
+          .children("video")
+          .trigger("pause");
+        $(this).parent(".vid").siblings(".vid").children(".blackLayer").show();
+        $(this).parent(".vid").siblings(".vid").children("svg").show();
+      }
+      $(this).siblings("video").trigger("play");
+      $(this).hide();
+      $(this).siblings(".blackLayer").hide();
+    });
+});
